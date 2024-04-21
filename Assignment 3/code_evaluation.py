e3ad244b264cf0a7914195c0cded1ad4cbecd7c6
kudhru <gargdhruv36@gmail.com>
2024-04-21 21:54:41 +0530

    make required changes for running assignment 3 smoothly

--- Assignment 3/code_evaluation.py
+++ Assignment 3/code_evaluation.py
@@ -11,9 +11,11 @@ import sys
 import openai
 from dotenv import load_dotenv
 import textwrap
-
+from datetime import datetime
 load_dotenv()
+import warnings
 
+warnings.filterwarnings("ignore")
 
 def code_Evaluation(question,code_snippet,VectorStore):
     query_for_code_eval = f'''
@@ -24,13 +26,11 @@ def code_Evaluation(question,code_snippet,VectorStore):
     3. Omissions: Discuss any requirements or specifications from the assignment that are missing in the code snippet. Mention what should be added to meet the assignment's complete criteria.
     4. Overall Evaluation: Provide a concise overall assessment of the code snippet, summarizing its strengths and weaknesses.
 
-    Rubric: {question}
-    Code Snippet: {code_snippet}
+    Rubric to be followed for evaluating the above mentioned criteria: {question}
+    Code Snippet provided by the student: {code_snippet}
 
     Evaluate the provided code snippet based on these criteria and provide detailed feedback on its functionality. Mention any additional considerations or improvements that could enhance the code's effectiveness or efficiency.
     '''
-
-
 
     llm = OpenAI(api_key=os.getenv('API_KEY'))
     chain = load_qa_chain(llm=llm, chain_type="stuff")
@@ -47,7 +47,7 @@ def code_summarization(code_snippet):
 
     2. Relevant Questions: Generate two questions based on the code snippet that test the student's understanding and ability to write similar code.
 
-    Code Snippet: {code_snippet}
+    Code Snippet provided by the student: {code_snippet}
 
     Please provide a clear and concise summary and formulate questions that can help assess the depth of the student's understanding of the concepts implemented in the code snippet.
     '''
@@ -55,15 +55,15 @@ def code_summarization(code_snippet):
     llm = openai.OpenAI(api_key=os.getenv('API_KEY'))
     messages = [{'role': 'system', 'content': '''You are a teaching assistant bot tasked with providing additional insights into a student's programming assignment submission. Your response should include the following components:
     1. Code Summary: Provide a summary of what the code is intended to do. Describe the main functionality and logic of the code snippet in 5-7 lines, focusing on key operations and the flow of data.
-    2. Relevant Questions: Generate two questions based on the code snippet that test the student's understanding and ability to write similar code.'''}]
+    2. Relevant Questions: Generate two questions based on the code snippet that test the student's understanding and ability to write similar code. Please also generate expected answers for each question. Please state the expected answers in brief.'''}]
 
     messages.append({'role': 'user', 'content': query_for_additional_insights})
     response = llm.chat.completions.create(model="gpt-4-turbo", messages=messages)
 
     return response.choices[0].message.content
 
-def write_to_file(question, evaluation_response, summarization_response, output_file):
-    with open("output.txt", "a") as output_file:
+def write_to_file(question, evaluation_response, summarization_response, output_file_path):
+    with open(output_file_path, "a") as output_file:
         wrapper = textwrap.TextWrapper(width=100)
         lines = question.split("\n")
         for line in lines:
@@ -79,14 +79,16 @@ def write_to_file(question, evaluation_response, summarization_response, output_
         for line in lines:
             output_file.write(wrapper.fill(line) + "\n")
         output_file.write("\n\n")
+        output_file.write("----------------------------------------------------------------\n")
+        output_file.write("----------------------------------------------------------------\n")
 
 def main():
+    group_number=input("Enter the group number:")
     rubric = 'rubric.json'
 
     with open(rubric,"r") as fd:
         data=json.load(fd)
-
-    pdfs = {'Assignemt': 'W24 CSE530 DSCD Assignment 3.pdf', 'Map Reduce for K-Means Algorithm': 'MapReduce for K-Means Clustering.pdf'}
+    pdfs = {'Assignemt': 'W24 CSE530 DSCD Assignment 3 - For Evaluation using LLMs.pdf', 'Map Reduce for K-Means Algorithm': 'MapReduce for K-Means Clustering.pdf'}
 
     sup_text = ""
 
@@ -111,20 +113,32 @@ def main():
     embeddings = OpenAIEmbeddings(api_key=os.getenv('API_KEY'))
     VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
+    timestamp = datetime.now().strftime("%m-%d_%H-%M-%S")
+    output_file_path = os.path.join("output_files", f"output_code_evaluation_group_id_{group_number}_time_{timestamp}.txt")
+    # output_file = open(output_file_path, "a")
     for q in data:
-        ques=q["question"]
-        pause=input(f'''Please paste the code snippet for
-                        Question:  {ques}
+        # ques=q["question"]
+        functionality_id=q["functionality_id"]
+        functionality_tag=q["functionality_tag"]
+        code_snippet_file_path=f"codesnippet_functionality_id_{functionality_id}.txt" 
+        pause=input(f'''Please paste the code snippet in {code_snippet_file_path} file for
+                        Functionality Id:  {functionality_id}
+                        Functionality Tag:  {functionality_tag}
                         After that press [Enter] to continue...''')
         
-        with open("codesnippet.txt","r") as fc:
-            code=fc.read()
+    for q in data:
+        ques=q["question"]
+        functionality_id=q["functionality_id"]
+        code_snippet_file_path=f"codesnippet_functionality_id_{functionality_id}.txt"
 
-        output_file = open("output.txt", "a")
+        with open(code_snippet_file_path,"r") as fc:
+            code=fc.read()
+        print(f"Evaluating code block for Functionality ID {functionality_id} \n")
         evaluation_response = code_Evaluation(ques, code, VectorStore)
         summarization_response = code_summarization(code)
 
-        write_to_file(ques, evaluation_response, summarization_response, output_file)
+        write_to_file(ques, evaluation_response, summarization_response, output_file_path)
+        print(f"Evaluation finished for code block for Functionality ID {functionality_id} \n")
 
 if __name__ == "__main__":
     main()
--- Assignment 3/question_generation_new.py
+++ Assignment 3/question_generation_new.py
@@ -1,6 +1,4 @@
 from dotenv import load_dotenv
-import pickle
-from PyPDF2 import PdfReader
 from datetime import datetime
 from langchain_openai import OpenAIEmbeddings
 
@@ -11,49 +9,25 @@ from langchain_community.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
 import os
-import time
 import random
 import PyPDF2
 import json
-def evaluate(students_answer,VectorStore,llm_res):
-    
-    evaluation_query=f'''You have to give marks to the student out of 6 .(Each question has 2 marks) Below are the students and teachers response.
-            Students response: {students_answer}
-            Teachers response:{llm_res}
-            If the answer is irrelevant then give zero.Display marks given to each question and then the total final marks .'''
-    grade=ask_question(evaluation_query,VectorStore)
-def split(question_answer:str):
-    ind_questionmark=question_answer.find("?")
-    question=question_answer[:ind_questionmark+1]
-    answer=question_answer[ind_questionmark+1:]
-    return question,answer
 
-# def read_pdf(pdf_path):
-#     # Open the PDF file
-#     with open(pdf_path, 'rb') as file:
-#         # Create a PDF reader object
-#         pdf_reader = PyPDF2.PdfReader(file)
-        
-#         # Read each page of the PDF
-#         text = ''
-#         for page_num in range(len(pdf_reader.pages)):
-#             page = pdf_reader.pages[page_num]
-#             text += page.extract_text()
-        
-#         return text
+
 def read_pdf(pdf_path):
     # Open the PDF file
     with open(pdf_path, 'rb') as file:
         # Create a PDF reader object
         pdf_reader = PyPDF2.PdfReader(file)
-        
+
         # Read each page of the PDF
         text = ''
         for page_num in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_num]
             text += page.extract_text()
-        
+
         return text
+
 
 def read_multiple_pdfs(pdf_paths):
     all_texts = []
@@ -63,166 +37,114 @@ def read_multiple_pdfs(pdf_paths):
     return all_texts
 
 
-    
-
-
-
-
-    return grade
-
-def format_query(query:str):
-    l=query.split(', ')
-    s="\n".join(l)
+def format_query(query: str):
+    l = query.split(', ')
+    s = "\n".join(l)
     return s
+
+
 def choose(list_topics):
     return random.choice(list_topics)
-def randomize_list(list_topics:list):
-    x=list_topics
-    new_list=[]
+
+
+def randomize_list(list_topics: list):
+    x = list_topics
+    new_list = []
     for i in range(len(x)):
-        choosed_item=choose(list_topics)
+        choosed_item = choose(list_topics)
         new_list.append(choosed_item)
         list_topics.remove(choosed_item)
-    topics_choosen=new_list
-    # topics_choosen=[]
-    # for i in range(5):
-
-    #     topic=choose(new_list)
-    #     new_list.remove(topic)
-    #     topics_choosen.append(topic)
+    topics_choosen = new_list
     return topics_choosen
 
-    
-def ask_question(query,VectorStore):
-    
-    
+
+def prompt_llm(query, VectorStore):
     docs = VectorStore.similarity_search(query=query, k=3)
     load_dotenv()
-    llm = OpenAI(api_key='YOUR_API_KEY')
+    llm = OpenAI(api_key=os.getenv('API_KEY'))
     chain = load_qa_chain(llm=llm, chain_type="stuff")
     with get_openai_callback() as cb:
         response = chain.run(input_documents=docs, question=query)
-            
-        
-
     return response
- 
+
+
 load_dotenv()
- 
+
+
 def main():
     print("Viva Questions")
-    group_number=input("Enter the group number:")
-    num_questions=int(input("Enter the number of questions you wish to generate: "))
- 
- 
+    group_number = input("Enter the group number:")
+    num_questions = int(input("Enter the number of questions you wish to generate: "))
+
     # upload a PDF file
     pdf = ["W24 CSE530 DSCD Assignment 3.pdf"]
     # pdf = ["VivaBot - W24 CSE530 DSCD Assignment 2.pdf","raft.pdf"]
-    text_list=read_multiple_pdfs(pdf)
-    text=''''''
-    pdf_id=0
-    pdf_topics=["Assignment PDF"]
+    text_list = read_multiple_pdfs(pdf)
+    text = ''''''
+    pdf_id = 0
+    pdf_topics = ["Assignment PDF"]
     # pdf_topics=["Assignment PDF","Raft Paper PDF"]
     for t in text_list:
         # text=text+f'''PDF Topic:{pdf_topics[pdf_id]}\n\n{t}\nNext Pdf\n'''
-        text=text+f'''PDF Topic:{pdf_topics[pdf_id]}\n\n{t}\n'''
-        pdf_id+=1
+        text = text + f'''PDF Topic:{pdf_topics[pdf_id]}\n\n{t}\n'''
+        pdf_id += 1
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
-        )
+    )
     chunks = text_splitter.split_text(text=text)
     load_dotenv()
-    embeddings = OpenAIEmbeddings(api_key='YOUR_API_KEY')
+    embeddings = OpenAIEmbeddings(api_key=os.getenv('API_KEY'))
+    # llm = OpenAI(api_key=os.getenv('API_KEY'))
 
     VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
-    
-    import_topics=['Raft', 'Leader Election in Raft', 'Log Replication in Raft', 'RequestVote RPC', 'AppendEntry RPC', 'Leader Lease in Raft', 'Heartbeats in Raft' , 'Fault Tolerance in Raft']
+    important_topics = ["MapReduce", "Map phase in MapReduce", "Reduce phase in MapReduce", "Partition phase in MapReduce",
+                     "Shuffle phase in MapReduce", "Sort phase in MapReduce"]
 
-    
-
-    if import_topics!=[]:
-        
-            
-        questions=[]
-        topics_choosen=randomize_list(import_topics)
-        c=0
-        d = 0
-        # for i in range(num_questions):
-        #     if c>=len(topics_choosen):
-        #         c=0
-                
-        #     q2=f'''Generate one different,very detailed and technical question  along with their answer (IN SEPERATE LINES) STRICTLY from the text.Question should be strictly from the topic given below:
-        #                                 TOPIC:
-        #                                 {topics_choosen[c]}
-        #                         .Do NOT REPEAT THE PREVIOUSLY ASKED QUESTIONS '''
-            
-        #     q3=f'''Generate one different,very detailed question where a possible scenario is generated in the form of a question along with their answer (IN SEPERATE LINES) STRICTLY from the text.Question should be strictly from the topic given below:
-        #                                 TOPIC:
-        #                                 {topics_choosen[c]}
-        #                         .Do NOT REPEAT THE PREVIOUSLY ASKED QUESTIONS '''
-        #     response1=ask_question(q2,VectorStore)
-        #     questions.append(response1)
-        #     response2=ask_question(q3,VectorStore)
-        #     questions.append(response2)
-        #     c+=1
-        # j = 0
-        # if (num_questions%2 == 0):
-        #     j = num_questions/2
-        # else:
-        #     j = (num_questions+1)//2 
-        for i in range(num_questions//2):
-            if c >= len(topics_choosen):
-                c = 0
-
-            q2 = f'''Generate one different, very detailed and technical question along with their answer (IN SEPARATE LINES) STRICTLY from the text. Question should be strictly from the topic given below:
+    if important_topics:
+        questions = []
+        answers = []
+        topics_choosen = randomize_list(important_topics)
+        topic_index = 0
+        for i in range(num_questions):
+            if topic_index >= len(topics_choosen):
+                topic_index = 0
+            prompt_to_llm = f'''You are a teaching assistant for distributed systems class. You need to take viva of a student in which the student will be asked a question and they will be given 3 minutes to write the answer for this question. Please generate a question strictly from the topic given below:
                 TOPIC:
-                {topics_choosen[c]}
+                {topics_choosen[topic_index]}
                 .Do NOT REPEAT THE PREVIOUSLY ASKED QUESTIONS'''
-                
-            response1 = ask_question(q2, VectorStore)
-            questions.append(response1)
-            c += 1
-        
-        for i in range(num_questions- (num_questions//2)):
-            if d >= len(topics_choosen):
-                d = 0
-                
-            q3 = f'''Generate one different, very detailed question where a possible scenario is generated in the form of a question along with their answer (IN SEPARATE LINES) STRICTLY from the text. Question should be strictly from the topic given below:
-                TOPIC:
-                {topics_choosen[c]}
-                .Do NOT REPEAT THE PREVIOUSLY ASKED QUESTIONS'''
-                
-            response2 = ask_question(q3, VectorStore)
-            questions.append(response2)
-            c += 1
+            response = prompt_llm(prompt_to_llm, VectorStore)
+            questions.append(response)
 
-            # student_response=st.empty()
-        al_questions="\n\n\n".join(questions)
-        new_q=[]
-        new_a=[]
-        for q in questions:
-            q1,a1=split(q)
-            new_q.append(q1)
-            new_a.append(a1)
-        for i in range(len(new_q)):
-            print(f"Question {i+1}:{new_q[i]}")
-        final_list_dictionary=[]
-        for i in range(len(new_q)):
-            d={}
-            d["question"]=new_q[i]
-            d["model_answer"]=new_a[i]
-            d["student_answer"]=""
-            d["TA_rating_for_question_usefulness"]=""
-            d["TA_comments_for_question_usefulness"]=""
+            prompt_to_llm = f'''Please answer the following question in at most 5-7 lines:
+                {response}'''
+            response = prompt_llm(prompt_to_llm, VectorStore)
+            answers.append(response)
+
+            topic_index += 1
+
+        for i in range(len(questions)):
+            print(f"Question {i + 1}:{questions[i]}")
+        final_list_dictionary = []
+
+        for i in range(len(questions)):
+            d = {
+                "question": questions[i],
+                "model_answer": answers[i],
+                "student_answer": "",
+                "TA_rating_for_question_usefulness": "",
+                "TA_comments_for_question_usefulness": ""
+            }
             final_list_dictionary.append(d)
             timestamp = datetime.now().strftime("%m-%d_%H-%M-%S")
-            question_file_path = os.path.join("question_files", f"students_answers_group_id_{group_number}_time_{timestamp}.json")
+            question_file_path = os.path.join("viva_question_files",
+                                              f"students_answers_group_id_{group_number}_time_{timestamp}.json")
 
             with open(question_file_path, "w", encoding="utf-8") as json_file:
                 json.dump(final_list_dictionary, json_file, indent=4, ensure_ascii=False)
+
 
 if __name__ == '__main__':
     main()
